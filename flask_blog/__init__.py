f5f6e55ac44944988768e9007f7d1952f04655d3
[Wilfred Owobu] <owobuwilfred@gmail.com>
2021-08-12 15:48:45 +0100

    additional commit

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,6 +1,8 @@
 from logging import DEBUG
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 
@@ -11,5 +13,8 @@ app.config['SECRET_KEY'] = '63f4ce6948a9d40a72191bb94b9c23fb'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 from flask_blog import routes
--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm 
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired,Email,EqualTo,Length
+from wtforms.validators import DataRequired,Email,EqualTo,Length,ValidationError
+from flask_blog.models import User
 
 
 
@@ -10,6 +11,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password',validators =[DataRequired(),Length(min = 8)])
     confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken, Please choose a different username')
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken, Please choose a different email')
 
 
 class LoginForm(FlaskForm):
