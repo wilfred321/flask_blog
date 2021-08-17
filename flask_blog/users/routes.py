7aa9a9624a4997c690d25588b219a66450a3d9e6
[Wilfred Owobu] <owobuwilfred@gmail.com>
2021-08-17 20:15:32 +0100

    edited url_for

--- flask_blog/users/routes.py
+++ flask_blog/users/routes.py
@@ -17,7 +17,7 @@ users = Blueprint("users", __name__)
 @users.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for("home"))
+        return redirect(url_for("main.home"))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
@@ -29,21 +29,21 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash("Your account has been created! You are now able to login", "success")
-        return redirect(url_for("login"))
+        return redirect(url_for("users.login"))
     return render_template("register.html", title="Register", form=form)
 
 
 @users.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("home"))
+        return redirect(url_for("main.home"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get("next")
-            return redirect(next_page) if next_page else redirect(url_for("home"))
+            return redirect(next_page) if next_page else redirect(url_for("main.home"))
         else:
             flash("Login Unsuccessful, Please check email and password!", "danger")
     return render_template("login.html", title="Login", form=form)
@@ -53,7 +53,7 @@ def login():
 def logout():
     logout_user()
     flash("You have successfully logged out.", "success")
-    return redirect(url_for("home"))
+    return redirect(url_for("main.home"))
 
 
 @users.route("/account", methods=["GET", "POST"])
@@ -68,7 +68,7 @@ def account():
         current_user.email = form.email.data
         db.session.commit()
         flash("Your account was updated successfully!", "success")
-        return redirect(url_for("account"))
+        return redirect(url_for("users.account"))
 
     elif request.method == "GET":
         form.username.data = current_user.username
