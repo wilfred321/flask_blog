dbe8ba49164fbcd0e2f418384dd47af3174274fa
[Wilfred Owobu] <owobuwilfred@gmail.com>
2021-08-17 21:36:39 +0100

    Added config object

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -4,15 +4,11 @@ from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
-import os
+from flask_blog.config import Config
 
 
 app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config["SECRET_KEY"] = "63f4ce6948a9d40a72191bb94b9c23fb"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("FLASKBLOG_DB_URL")
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
@@ -20,11 +16,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = "users.login"
 login_manager.login_message_category = "info"
 
-app.config["MAIL_SERVER"] = "smtp.googlemail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.getenv("EMAIL_USER")
-app.config["MAIL_PASSWORD"] = os.getenv("EMAIL_PASS")
+
 mail = Mail(app)
 
 from flask_blog.users.routes import users
---
+++ flask_blog/config.py
@@ -0,0 +1,14 @@
+import os
+
+
+class Config:
+    TEMPLATES_AUTO_RELOAD = True
+    SECRET_KEY = "63f4ce6948a9d40a72191bb94b9c23fb"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("FLASKBLOG_DB_URL")
+    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv("EMAIL_USER")
+    MAIL_PASSWORD = os.getenv("EMAIL_PASS")
