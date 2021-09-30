53da21db3d47d34697d875360b482d610f85afd5
[Wilfred Owobu] <owobuwilfred@gmail.com>
2021-09-30 16:55:47 +0100

    removed sqlaclchemy database url

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = "63f4ce6948a9d40a72191bb94b9c23fb"
     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv("FLASKBLOG_DB_URL")
+    # SQLALCHEMY_DATABASE_URI = os.getenv("FLASKBLOG_DB_URL")
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
