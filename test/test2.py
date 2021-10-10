26959937d66d5d5c25a614bf38363eb21d53e6b5
[Wilfred Owobu] <owobuwilfred@gmail.com>
2021-10-10 08:34:47 +0100

    addition

---
+++ test/test2.py
@@ -0,0 +1,71 @@
+# from random import shuffle
+# from random import randrange
+
+# names = ["Ken", "Tim", "Joe", "Kelly"]
+# rand_num = randrange()
+# print(rand_num)
+
+# import random
+
+# random_list = random.sample(range(0, 9), 6)
+# print(random_list)
+# new = random_list.join(",")
+# print(new)
+
+
+# numbers = "".join(random_list)
+# print(numbers)
+
+# names = ["Ken", "Tim", "Joe", "Kelly"]
+
+
+# new_names = "".join(names)
+
+# print(new_names)
+
+
+# import random
+# import string
+
+# numbers = string.digits
+
+# print("".join(random.choice(numbers) for i in range(6)))
+
+
+# # printing lowercase
+# letters = string.ascii_lowercase
+# print("".join(random.choice(letters) for i in range(10)))
+
+# # printing uppercase
+# letters = string.ascii_uppercase
+# print("".join(random.choice(letters) for i in range(10)))
+
+# # printing letters
+# letters = string.ascii_letters
+# print("".join(random.choice(letters) for i in range(10)))
+
+# # printing digits
+# letters = string.digits
+# print("".join(random.choice(letters) for i in range(10)))
+
+# # printing punctuation
+# letters = string.punctuation
+# print("".join(random.choice(letters) for i in range(10)))
+
+# name = "Desmond"
+# if name == "Desmond":
+#     print(f"Name is {name}")
+
+# print(f"Name is not {name}")
+
+import bcrypt
+
+
+# password = b"super secret password"
+
+# hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+# if bcrypt.checkpw(password, hashed_password):
+#     print("It matches")
+# else:
+#     print("It does not match!")
